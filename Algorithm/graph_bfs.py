5828d2c14a22be5dfcadcde5058dd840118ffa06
YChaeeun <se_same5315@naver.com>
2019-03-18 19:18:32 +0900

    graph

--- Algorithm/graph_bfs.py
+++ Algorithm/graph_bfs.py
@@ -1,18 +1,26 @@
 
 def search_bfs(graph, start) :
     qu = list()
-    visited = set()
+    visited = set()  
+
+    result = list()
+    # list보다 빠른 검색 속도, 그렇지만 순서가 없는 자료형이라 return visited 하면 안돼
+    # 1) 값을 저장할 result 리스트를 따로 만들던지, 2) 결과를 바로바로 프린트하던지, 3) visited를 set말고 list로 구현한뒤 return 하던지
 
     qu.append(start)
+    result.append(start)
     visited.add(start)
 
     while qu :
         node = qu.pop(0)
+        print(node)  # 대신 바로 print
         for x in graph[node] :
             if x not in visited :
                 qu.append(x)
+                result.append(x)
                 visited.add(x)
-    return visited
+    
+    return result
 
 
 graph = {
